83fc5a817ed6a7fbbc6e34e636827c263167fcd9
ImDev11 <euris110811@gmail.com>
2023-04-05 21:02:03 -0400

    @mui/styles added for to use makeStyles function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Container, TextField } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+  }
+});
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Hello world
-    </div>
+    <Container className={classes.container} maxWidth="sm">
+      <TextField id="standard-basic" label="Task" variant="standard" />
+    </Container>
   );
 }
 
